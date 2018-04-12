2ad0d97ae084f8fb1705142def4db1daf89ae6f6
Andrew McLaughlin <andrew@mclau.ghlin.com>
2018-04-12 16:11:20 -0700

    Added ability to pass vars directly to script

--- from-vagrant-copy.sh
+++ from-vagrant-copy.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
+set -x
 
-echo "Vagrant box username?"
-read username
+username=$1
+guest_path=$2
+host_path=$3
 
-echo "Vagrant box (guest) file path?"
-read guest_path
+question() {
+    message=$1
+    var=$2
+    echo $message
+    read answer
+    eval ${var}=${answer}
+}
 
-echo "Local (host) file path?"
-read host_path
+ask_questions() {
+    question "Vagrant box username?" username
+    question "Vagrant box (guest) file path?" guest_path
+    question "Local (host) file path?" host_path
+}
 
-scp -rP 2222 ${username}@127.0.0.1:${guest_path} ${host_path}
+main() {
+    if [ -z $username ] &&
+       [ -z $guest_path ] &&
+       [ -z $host_path ]
+    then
+        ask_questions
+    fi
+    scp -rP 2222 ${username}@127.0.0.1:${guest_path} ${host_path}
+}
 
-
+main
